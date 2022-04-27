03dce8de67c65c901a4192a30d45cdb97a2ba9fa
Izabella Williams <iza.yy10@gmail.com>
2022-04-27 01:26:33 -0400

    last minute properties

--- src/shared/classes/in game/vessels/parts/VesselPart.ts
+++ src/shared/classes/in game/vessels/parts/VesselPart.ts
@@ -3,11 +3,17 @@ import { StorageContainer } from "../../resources/StorageContainer";
 
 export class VesselPart
 {
-	constructor (StorageOfPart: StorageContainer, ResourcesConsumedByPart: Resource[])
+	constructor (ModelOfPart: Model, WeightInKG: number, StorageOfPart: StorageContainer, ResourcesConsumedByPart: Resource[])
 	{
+		this.ModelOfPart = ModelOfPart;
+		this.WeightInKG = WeightInKG;
 		this.StorageOfPart = StorageOfPart;
 		this.ResourcesConsumedByPart = ResourcesConsumedByPart;
 	}
+
+	ModelOfPart: Model;
+
+	WeightInKG: number;
 
 	StorageOfPart: StorageContainer;
 
